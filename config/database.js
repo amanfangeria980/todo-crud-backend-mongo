5c84b0526fce87f3194271951c0fecd7b2ac057e
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-12 18:01:33 +0530

    did the db connection function work

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+
+// To use the .env file here, we have to install dotenv and write the following line
+
+require("dotenv").config();
+
+const dbConnect=()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }).then(()=>console.log("DB Connection Successful"))
+    .catch((err)=>{
+        console.log("Issue in DB Connection");
+        console.error(error.message);
+        process.exit(1);
+    })
+}
+
+module.exports=dbConnect;
