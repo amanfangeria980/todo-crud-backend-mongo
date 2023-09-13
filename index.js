3e06ac575ea24ce9861279eb06e85485bdb40c62
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-13 17:32:37 +0530

    the whole hierarchy to follow from 0

--- index.js
+++ index.js
@@ -32,4 +32,18 @@ app.get("/",(req,res)=>{
     res.send("<h1>This is HOMEPAGE </h1>")
 })
 
-// Finally, we are able to create a todo entry to our database
+/* 
+Hierarchy
+-Created the env file and added the env variables
+-Under config-> dbConnect function and exported it
+-Under models-> Created a schema and exported it
+-Under Controller-> imported the schema and create the business logic for createTodo and exported it as an asynchronous function
+-under routes-> imported the express, add the router, imported the route's business logic from controller and defined/mapped api routes and exported the whole file as router
+-under the main file index.js -> initiated the express server, go the port, used express.json() parser, imported the routes, mounted those routes using :- app.use("/api/v1",todoRoutes); and then started the server, imported the dbConnect function from config folder and started the db server and then created a default route for the home page
+-Finally, wrote the whole hierarchy
+ */
+
+
+
+
+
